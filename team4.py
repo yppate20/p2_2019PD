8dddc80a5d2a16c44947b32579f9785ba02478ea
yppate20 <47324252+yppate20@users.noreply.github.com>
2019-03-07 09:31:15 -0500

    Update team4.py

--- team4.py
+++ team4.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Team 4 check' # Only 10 chars displayed.
+strategy_name = ' Betray then Switch'
+strategy_description = 'By coosing bertray we will always gain 3 points or lose 1 point.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,6 +17,12 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
+    if len(my_history) == 0 :
+        return 'b'
+    elif  my_history[-1] == 'b' : 
+        return 'c'
+    else :
+        return 'b'
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -26,7 +32,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+    # return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
